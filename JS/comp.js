752d417163b92fbcedc3786ab4f6e0ad0acc1627
jeesssik <jeesssik@gmail.com>
2020-03-26 17:44:41 -0300

    patron telefono

--- JS/comp.js
+++ JS/comp.js
@@ -1,40 +1,28 @@
-let nombre= document.getElementById("nombre");
-let apellido= document.getElementById("apellido");
-let mail= document.getElementById("Email");
-let dni= document.getElementById("dni");
-let tel= document.getElementById("phone");
-
-let cod= document.getElementById(Select1)
 
 
 
-function chequear(){
-	if(nombre.value==="" || nombre.value===" "){
-		
-		nombre.style.border=" 2px solid red"
-	}
-	if(apellido.value==="" || apellido.value===" "){
-		
-		apellido.style.border=" 2px solid red"
-	}
-	if(mail.value==="" || mail.value===" "){
-		
-		mail.style.border=" 2px solid red"
-	}
-	if(dni.value==="" || dni.value===" "){
-		
-		dni.style.border=" 2px solid red"
-	}
-	if(tel.value==="" || tel.value===" "){
-		
-		tel.style.border=" 2px solid red"
-	}
 
+
+function corroborar(){
+	
+	document.getElementById("Select1").value
+	   
+	 if(parseInt(document.getElementById("Select1").value)==1){
+	 	document.getElementById("selector2").style.display = "none";
+	  	document.getElementById("selector3").style.display = "inline";
+	  	
+	  }else
+
+	  if(parseInt(document.getElementById("Select1").value)==24){
+	  	document.getElementById("selector2").style.display = "inline";
+	  	document.getElementById("selector3").style.display = "none";
+	  }else 
+	  if(parseInt(document.getElementById("Select1").value)!=1 && parseInt(document.getElementById("Select1").value)!=24 ){
+	  	document.getElementById("selector3").style.display = "none";
+	  	document.getElementById("selector2").style.display = "none";
+	  }
+	   
 }
-
-
-
-
 
 
 
